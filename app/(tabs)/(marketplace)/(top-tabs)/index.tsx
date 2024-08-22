e5589a41eca9fbb02fe1e83a65c001496fec0c1b
Jackie Lin <jackielin2025@u.northwestern.edu>
2024-08-22 13:11:57 -0400

    community icon

--- app/(tabs)/(marketplace)/(top-tabs)/index.tsx
+++ app/(tabs)/(marketplace)/(top-tabs)/index.tsx
@@ -1,9 +1,11 @@
+import ProfileIcon from '@/components/ProfileIcon';
 import { StyleSheet, Text, View } from 'react-native';
 
 const ExploreTab = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <ProfileIcon variant="community" />
     </View>
   );
 };
