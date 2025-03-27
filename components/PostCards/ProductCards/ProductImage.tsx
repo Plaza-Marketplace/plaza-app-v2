e921614766b4c6ba7d1c3b1a9bd48ecee58cfd93
kyeoul <54390304+kyeoul@users.noreply.github.com>
2025-03-27 17:55:39 -0500

    git config core.ignorecase...??

---
+++ components/PostCards/ProductCards/ProductImage.tsx
@@ -0,0 +1,24 @@
+import { View } from 'react-native';
+import React from 'react';
+import productCardStyles from './styles';
+import { Image } from 'expo-image';
+
+const ProductImage = ({ uri }: { uri: string }) => {
+  return (
+    <View style={productCardStyles.shadow}>
+      <View style={[productCardStyles.background]}>
+        <Image
+          style={{
+            flex: 1,
+            width: '100%',
+            backgroundColor: '#0553',
+          }}
+          source={uri}
+          contentFit="cover"
+        />
+      </View>
+    </View>
+  );
+};
+
+export default ProductImage;
---
+++ components/PostCards/ProductCards/ProductSelectedShowcase.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Radius from '@/constants/Radius';
+import StandardText from '@/components/Texts/StandardText';
+import MediumText from '@/components/Texts/MediumText';
+import Color from '@/constants/Color';
+import ProductImage from './ProductImage';
+import Spacing from '@/constants/Spacing';
+import productCardStyles from './styles';
+import { ProductDetails } from '@/models/communityPost';
+
+interface ProductSelectedShowcase {
+  product: ProductDetails;
+}
+
+const ProductSelectedShowcase: FC<ProductSelectedShowcase> = ({ product }) => {
+  return (
+    <View style={[styles.card, productCardStyles.shadow]}>
+      <ProductImage uri={product.imageUrls[0]} />
+      <View style={styles.textContainer}>
+        <StandardText>{product.name}</StandardText>
+        <MediumText>{`$${product.price}`}</MediumText>
+      </View>
+    </View>
+  );
+};
+
+export default ProductSelectedShowcase;
+
+const styles = StyleSheet.create({
+  textContainer: {
+    gap: Spacing.SPACING_1,
+  },
+  card: {
+    width: '100%',
+    padding: Spacing.SPACING_3,
+    backgroundColor: Color.SURFACE_PRIMARY,
+    borderColor: Color.BORDER_SECONDARY,
+    borderRadius: Radius.ROUNDED,
+    borderWidth: 1,
+    gap: Spacing.SPACING_1,
+  },
+});
